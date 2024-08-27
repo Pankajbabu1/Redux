a18d6c3f88e98dde17a3e0e0c9023338c65905f9
Pankaj Rai <pankajrai8887@gmail.com>
2024-08-27 19:08:20 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Redux from './Redux';
+
+function App() 
+{
+  return (
+    <div className="App">
+    <Redux/>
+    </div>
+  );
+}
+
+export default App;
